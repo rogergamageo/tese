2165a5f68a226d7e7677aa20f6e6c3d7229d3cd0
rogerio.gama <rogeriogeografia@gmail.com>
2022-04-01 09:44:04 -0300

    primeira versão do código de requisição

--- Dados/reqsigeo.py
+++ Dados/reqsigeo.py
@@ -1,10 +1,20 @@
-"Função para baixar dados do SIGEO-Niteroi-Dados Abertos"
+# Função para baixar dados do SIGEO-Niteroi-Dados Abertos
+# a partir de uma lista com os nomes das camadas e a URL da API
+# salvar na pasta dados/sigeo
 
 import wget
 
-pasta = "/home/rogeriogama/Área de Trabalho/Projetos/Tese/Dados/"
+url = "https://opendata.arcgis.com/datasets/c5ba3f423a084e559cef63c07447cbb3_30.geojson"
+pasta = "/home/rogeriogama/Área de Trabalho/Projetos/tese/Dados/SIGEO/"
 nomea = "logradouro"
+
+# Função de requisição para a API SIGEO
 
 
 def baixar_sigeo(url, pasta, nomea):
     wget.download(url, pasta + nomea + ".geojson")
+
+# if __name__ == "main":
+
+
+baixar_sigeo(url, pasta, nomea)
