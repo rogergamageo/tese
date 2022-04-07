71edf98b411078976d71de832221f0ab373b505f
rogerio.gama <rogeriogeografia@gmail.com>
2022-04-07 10:46:19 -0300

    atualização codigo reqimagem

--- Dados/reqimagem.py
+++ Dados/reqimagem.py
@@ -1,21 +1,28 @@
 import wget
 import pandas as pd
+from zipfile import ZipFile
 
-pasta = "/home/rogeriogama/Área de Trabalho/Projetos/tese/Dados/SIGEO/"
+pasta = "/home/rogeriogama/Área de Trabalho/Projetos/tese/Dados/SIGEO/imagem"
 
 # Função de requisição para a API SIGEO
 
 
-def baixar_sigeo(url, pasta, nomea):
+def baixar_imagem(url, pasta, nomea):
+    wget.download(url, pasta + nomea + ".zip")
+    z = ZipFile(pasta+nomea+'.zip', 'r')
+    z.extractall(pasta)
+    z.close()
 
-    wget.download(url, pasta + nomea + ".tiff")
 
 # Baixar os dados a partir de uma listagem de url
+coluna = ['6907', '6923', '6939', '6955', '6971', '6987', '7003',
+          '7019', '7035', '7051', '7067', '7083', '7099', '7115']
+linha = ['74714', '74704', '74694', '74684', '74674', '74664', '74654', '74644',
+         '74634', '74624', '74614', '74604', '74594', '74584', '74574', '74564']
 
-
-df = pd.read_csv(
-    "/home/rogeriogama/Área de Trabalho/Projetos/tese/Dados/imagem_nit.csv", sep=';')
 # print(df)
-for x in range(len(df)):
-    if df['tipo'] == 'TIFF':
-        baixar_sigeo(df['url'][x], pasta, df['nomei'][x])
+for x in coluna:
+    for y in linha:
+        baixar_imagem("http://www.sigeo.niteroi.rj.gov.br/ortofotos/2019/TIFF/" +
+                      '{}-{}'.format(x, y) + "-TIFF.zip", pasta, '{}-{}'.format(x, y))
+        print('{}-{}'.format(x, y))
