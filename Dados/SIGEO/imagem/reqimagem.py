a1f164a859da5bcb117ba583884a5e668a48c620
rogerio.gama <rogeriogeografia@gmail.com>
2022-04-08 07:57:30 -0300

    commit

--- Dados/SIGEO/imagem/reqimagem.py
+++ Dados/SIGEO/imagem/reqimagem.py
@@ -22,11 +22,12 @@ coluna = ['6907', '6923', '6939', '6955', '6971', '6987', '7003',
 linha = ['74714', '74704', '74694', '74684', '74674', '74664', '74654', '74644',
          '74634', '74624', '74614', '74604', '74594', '74584', '74574', '74564']
 
-# print(df)
+# main
 for x in coluna:
     for y in linha:
         if(os.path.isfile(pasta+'{}-{}'.format(x, y)+'.zip')):
             print("O arquivo existe")
+            break
         else:
             try:
                 baixar_imagem("http://www.sigeo.niteroi.rj.gov.br/ortofotos/2019/TIFF/" +
